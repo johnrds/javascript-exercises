25bfdb011ab1850b650e6380117659ff0e769234
johnrds <121064478+johnrds@users.noreply.github.com>
2023-01-18 15:30:41 -0300

    Complete ex. 05_sumAll and pass all Jets tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function (intOne, intTwo) {
+    if (typeof (intOne) !== 'number' || intOne < 0) return 'ERROR';
+    if (typeof (intTwo) !== 'number' || intTwo < 0) return 'ERROR';
 
+    if (intOne > intTwo) {
+        const tempIntOne = intOne;
+        intOne = intTwo;
+        intTwo = tempIntOne;
+    };
+
+    let result = 0;
+    for (let i = intOne; i <= intTwo; i++) {
+        result += i;
+    };
+    return result;
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
