bc75dcf1237ade396fcbac29db3afad213ef76a2
johnrds <121064478+johnrds@users.noreply.github.com>
2023-01-18 16:26:45 -0300

    Complete ex. 09_palindromes and all Jets tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let forwardArray = string.toLowerCase().replace(/[^a-z]/g,'').split('');
+    let backwardArray = string.toLowerCase().replace(/[^a-z]/g,'').split('').reverse();
 
+    for(let i=0; i<forwardArray.length; i++){
+        if (forwardArray[i] === backwardArray[i]) {
+            continue;
+        } else {return false};
+    }
+    return true;
 };
 
 // Do not edit below this line
