a07a16a3b33b0ec9a138feaa30bc60aedf68053b
johnrds <121064478+johnrds@users.noreply.github.com>
2023-01-18 16:48:43 -0300

    Complete ex. 10_fibonacci and pass all Jest tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(int) {
+    if (isNaN(int) || int<0) return 'OOPS';
 
+    let result = [1,1];
+    if (int === 0) return 0;
+    for(let i=2; i<int; i++){
+        result.push(result[i-1]+result[i-2]);
+    };
+    return result[int-1];
 };
 
 // Do not edit below this line
