42782e65e16083931747414881aa67fb570dbb59
johnrds <121064478+johnrds@users.noreply.github.com>
2023-01-18 15:21:51 -0300

    Complete ex. 04_removeFromArray and all Jets tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
 const removeFromArray = function() {
+    const valuesToRemove = Array.from(arguments);
+    const myArray = valuesToRemove.shift();
 
+    for(let i=0; i<valuesToRemove.length; i++){
+        const valueIndex = myArray.indexOf(valuesToRemove[i]);
+        if (valueIndex === -1) continue;
+        myArray.splice(valueIndex,1);
+    };
+    return myArray
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 7, "tacos"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
