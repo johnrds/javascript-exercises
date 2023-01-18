48b7958d6e1dc2aaecb276cde7ea0f29f2b58fd6
johnrds <121064478+johnrds@users.noreply.github.com>
2023-01-18 15:05:16 -0300

    Complete ex. 02_repeatStrin and all Jets tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+    if(n < 0) return 'ERROR'
 
+    let result ='';
+    for(i=0; i<n; i++){
+        result +=string
+    };
+
+return result
 };
 
 // Do not edit below this line
