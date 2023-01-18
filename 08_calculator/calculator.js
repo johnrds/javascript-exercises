2b59e8d0e5190815038d741b66af49e2383972e6
johnrds <121064478+johnrds@users.noreply.github.com>
2023-01-18 15:43:54 -0300

    Complete ex 08_calculator and pass all Jets tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(intOne, intTwo) {
+	return intOne+intTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(intOne, intTwo) {
+	return intOne - intTwo;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+  for(number of array){
+    result += number;
+  };
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for(number of array){
+    result *= number;
+  };
+  return result;
 };
 
-const power = function() {
-	
+const power = function(intOne, intTwo) {
+	return intOne**intTwo;
 };
 
-const factorial = function() {
-	
+const factorial = function(int) {
+  if (int ===0) return 1;
+
+  let result = 1;
+  for(let i=1; i<=int; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
